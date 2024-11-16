d71bd8108ade5974111821c88d151250f8d9449c
Hamza <muhammadhamzam1486@gmail.com>
2024-11-16 21:37:05 +0500

    Initial Commit

---
+++ Nadra FaceLiveness Demo App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nadra FaceLiveness Demo App
+//
+//  Created by truID on 16/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nadra FaceLiveness Demo App/Nadra_FaceLiveness_Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nadra_FaceLiveness_Demo_AppApp.swift
+//  Nadra FaceLiveness Demo App
+//
+//  Created by truID on 16/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Nadra_FaceLiveness_Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
