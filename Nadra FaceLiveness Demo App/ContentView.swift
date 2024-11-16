1e1fc4e6921c692a38811a96f92575f48bba1b80
Hamza <muhammadhamzam1486@gmail.com>
2024-11-16 22:05:39 +0500

    Create the demo app for nadra face liveness

--- Nadra FaceLiveness Demo App/ContentView.swift
+++ Nadra FaceLiveness Demo App/ContentView.swift
@@ -6,19 +6,65 @@
 //
 
 import SwiftUI
+import TruID
 
 struct ContentView: View {
+    @State var isSDKRunning = false
+    @State var response: TruID.ResponseModel?
+    
+    @State var isReportScreenEnabled = true
+    @State var isHelpScreenEnabled = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        if isSDKRunning {
+            TruidMain(face_liveness: true, enableHelpScreens: isReportScreenEnabled, enableReportScreen: isHelpScreenEnabled, themeColor: Color.orange) { responseModel in
+                response = responseModel
+                isSDKRunning = false
+            } failure: { failure in
+                print(failure)
+                isSDKRunning = false
+            }
 
-#Preview {
-    ContentView()
+        } else {
+            VStack {
+                Text("Face Liveness Demo App")
+                    .font(.title)
+                    .padding(.bottom, 16)
+
+                if let response {
+                    if let result = response.result {
+                        HStack {
+                            Text("Liveness Passed:")
+                            Spacer()
+                            Image(systemName: result.status != "error" ? "checkmark.circle.fill" : "multiply.circle.fill")
+                        }
+                    }
+                    
+                    if let icao = response.icaoStatus {
+                        HStack {
+                            Text("ICAO Passed:")
+                            Spacer()
+                            Image(systemName: icao != "error" ? "checkmark.circle.fill" : "multiply.circle.fill")
+                        }
+                    }
+                    
+                    if response.status != 200 {
+                        Text("Something is wrong at the server.")
+                    }
+                }
+                
+                Spacer()
+                
+                Button {
+                    isSDKRunning = true
+                } label: {
+                    Label("Verify Liveness", systemImage: "shield.fill")
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.bordered)
+                .frame(maxWidth: .infinity)
+            }
+            .padding()
+        }
+    }
 }
